5fab9b5cba047db949d7161b6d90e517c9f199a4
simoncarda <165000637+simoncarda@users.noreply.github.com>
2026-03-15 17:41:16 +0100

    Základní .NET MAUI Blazor projekt s ukázkovými stránkami

---
+++ BPSnake/BPSnake/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BPSnake
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BPSnake/BPSnake/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BPSnake
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
