59c475773e300cc921324389b89127d856602d9a
simoncarda <165000637+simoncarda@users.noreply.github.com>
2026-04-06 18:31:42 +0200

    .

--- BP_Snake/Services/GameStateService.cs
+++ BP_Snake/Services/GameStateService.cs
@@ -5,8 +5,8 @@ namespace BPSnake.Services
 {
     /// <summary>
     /// Služba pro správu celkového stavu herní instance.
-    /// Demonstruje návrhový vzor State Machine - drží informaci o tom, co se s hrou aktuálně děje (Menu, Hraní, Pozastaveno, Konec)
-    /// a umožňuje čisté oddělení stavu od herní logiky (Enginu).
+    /// Drží informaci o tom, co se s hrou aktuálně děje (Menu, Hraní, Pozastaveno, Konec) a umožňuje čisté 
+    /// oddělení stavu od herní logiky (Enginu).
     /// </summary>
     internal sealed class GameStateService
     {
